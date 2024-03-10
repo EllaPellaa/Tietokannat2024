f3fb17ae6b2f98b9fb27932ae1c583aab7909942
EllaPellaa <112462853+EllaPellaa@users.noreply.github.com>
2024-03-10 16:45:59 +0200

    Tehtava17 gittiin

---
+++ Tehtava17/database.js
@@ -0,0 +1,8 @@
+const mysql = require('mysql2');
+const connection = mysql.createPool({
+  host: '127.0.0.1',
+  user: 'test_user',
+  password: 'test',
+  database: 'library'
+});
+module.exports = connection;
